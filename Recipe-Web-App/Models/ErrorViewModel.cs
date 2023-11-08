656c928f131e087805b6c6c365352cb77e7eeec0
Madsp15 <madsp@hotmail.dk>
2023-11-08 10:08:01 +0100

    Initial commit

---
+++ Recipe-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Recipe_Web_App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
