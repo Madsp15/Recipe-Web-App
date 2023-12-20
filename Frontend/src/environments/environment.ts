e019e6c865cadbffe51674546da249e31d9b8553
Madsp15 <madsp@hotmail.dk>
2023-12-20 16:23:38 +0100

    firebase

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://therecipewebsiteofalltime.azurewebsites.net/'
+  baseUrl: 'https://therecipewebsiteofalltime.azurewebsites.net'
 };
 
 /*
